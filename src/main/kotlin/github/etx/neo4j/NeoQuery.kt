3b6a7f7af8888cc9fd53a5c9b46fc95450f60cbf
brian <briankennedy392@gmail.com>
2018-07-22 19:57:18 -0400

    update versioning

--- src/main/kotlin/github/etx/neo4j/NeoQuery.kt
+++ src/main/kotlin/github/etx/neo4j/NeoQuery.kt
@@ -2,12 +2,6 @@ package github.etx.neo4j
 
 import org.neo4j.driver.internal.InternalRecord
 import org.neo4j.driver.v1.Driver
-import java.time.Instant
-import java.time.OffsetDateTime
-import java.time.OffsetTime
-import java.time.ZonedDateTime
-
-
 
 class NeoQuery(private val driver: Driver, private val serializer: INeoSerializer) {
 
